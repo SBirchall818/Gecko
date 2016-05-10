a21d3869cb7a9132aa0825cf05d8f4e866d32fc7
Simon Birchall <sbirchall@Simons-MacBook-Pro.local>
2016-05-10 19:04:25 +0100

    Set dial rotation to incoming data

--- public/dial/dial.directive.js
+++ public/dial/dial.directive.js
@@ -23,6 +23,7 @@
                 dialservice.getData().then(function(response) {
                     data = response.data;
                     setValuesToData(data);
+                    setRotation(data);
                 }, function (error) {
                     console.log('An ERROR occurred:\n' + error);
                 });
@@ -64,6 +65,28 @@
                     if (data.value !== undefined) {
                         scope.value = $filter(scope.format)(data.value, getCurrencySymbol(scope.unit), decimalPoints);
                     }
+                }
+
+                function setRotation(data) {
+                    var baseRotation = 35;
+
+                    // Checking that min <= value <= max
+                    if (data.max <= data.min) {
+                        return;
+                    }
+                    if (data.value > data.max) {
+                        return;
+                    }
+                    if (data.value < data.min) {
+                        return;
+                    }
+
+                    var fractionOfPi = (data.value - data.min) / (data.max - data.min);
+                    var degreesRoundDial = 180 * fractionOfPi;
+                    var degrees = degreesRoundDial + baseRotation;
+
+                    var needleElem = element.find('.needlepic');
+                    needleElem.css('transform','rotate(' + degrees + 'deg)');
                 }
             }
 
