a75f36d3e83853fff1364a926c92ce3e8994f8f8
Simon Birchall <sbirchall@Simons-MacBook-Pro.local>
2016-05-10 10:52:09 +0100

    Add data formatting information The filter has been applied in JavaScript instead of in html form so that the symbol can respond to the injected data

--- public/dial/dial.directive.js
+++ public/dial/dial.directive.js
@@ -1,5 +1,5 @@
 angular.module('app', [])
-    .directive('dial', function() {
+    .directive('dial', function($filter) {
         var directive = {
             replace: false,
             restrict: 'E',
@@ -8,11 +8,23 @@ angular.module('app', [])
         };
 
         function link(scope, element, attrs) {
-            scope.min = 0;
-            scope.max = 10;
-            scope.value = 5;
+            var decimalPoints = 0;
+
             scope.format = 'currency';
             scope.unit = 'GBP';
+            scope.min = $filter(scope.format)(0, getCurrencySymbol(scope.unit), decimalPoints);
+            scope.max = $filter(scope.format)(10, getCurrencySymbol(scope.unit), decimalPoints);
+            scope.value = $filter(scope.format)(5, getCurrencySymbol(scope.unit), decimalPoints);
+
+            // TODO: Break this out into a mappings module
+            function getCurrencySymbol(code) {
+                switch (code){
+                    case 'GBP':
+                        return '£';
+                    default:
+                        return '$';
+                }
+            }
         }
 
         return directive;
--- spec/dial/dial.spec.js
+++ spec/dial/dial.spec.js
@@ -1,10 +1,10 @@
 describe('dial directive', function() {
-    var compile, scope, templateCache, directiveElem, template;
+    var compile, scope, templateCache, directiveElem, template, filter;
 
     beforeEach(function() {
         module('app');
 
-        inject(function ($templateCache, $compile, $rootScope) {
+        inject(function ($templateCache, $compile, $rootScope, $filter) {
             templateCache = $templateCache;
 
             template = __html__['public/dial/dial.html'];   // How the file is referenced in the project root.
@@ -12,6 +12,8 @@ describe('dial directive', function() {
 
             compile = $compile;
             scope = $rootScope.$new();
+
+            filter = $filter;
         });
 
         directiveElem = getCompiledElement();
@@ -54,5 +56,33 @@ describe('dial directive', function() {
     it('should have the display information available on scope', function() {
         expect(scope.format).toBeDefined();
         expect(scope.unit).toBeDefined();
+    });
+
+    it('should apply display filter', function() {
+        // TODO
+        // Currentyly this test does not add much value because all it is
+        // doing is checking if the $filter function can work in two places (which it can)
+        // This test should be re-written when data is injeted to ensure display is correct
+        var sym = '£';
+        var decimalPoints = 0;
+        var filterType = 'currency';
+        scope.min = filter(filterType)(0, sym, decimalPoints);
+        scope.max = filter(filterType)(10, sym, decimalPoints);
+        scope.value = filter(filterType)(5, sym, decimalPoints);
+
+        var queryResult = directiveElem[0].querySelector('#value');
+        var wrappedQueryResult = angular.element(queryResult);
+
+        expect(wrappedQueryResult.text()).toEqual('£5');
+
+        queryResult = directiveElem[0].querySelector('#min');
+        wrappedQueryResult = angular.element(queryResult);
+
+        expect(wrappedQueryResult.text()).toEqual('£0');
+
+        queryResult = directiveElem[0].querySelector('#max');
+        wrappedQueryResult = angular.element(queryResult);
+
+        expect(wrappedQueryResult.text()).toEqual('£10');
     });
 });
